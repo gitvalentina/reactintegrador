c160e84a0109ca262535ef1369a9cdaf8840ba40
gitvalentina <vdouce@udesa.edu.ar>
2022-09-05 09:46:56 -0300

    estructura

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React  from "react"
+import "./Footer.css"
+
+//por ahora es sin estado, seguro despues hay que cambiarlo
+function Footer() {
+    return ( <footer className='piedepagina'> 
+
+    <footer>
+        Olivia Fodrini - Delfina Delgui - Valentina Douce  
+    </footer>
+    
+    <footer>
+        WEB &copy; 2022
+    </footer>
+
+    <footer>
+        Programacion 3
+    </footer>
+
+    <p  text-align='center'>
+            <img className='udesa' src="https://udesa.edu.ar/sites/all/themes/udesa/images/logo.jpg?v=01" alt="" />
+    </p>
+
+</footer>
+
+
+);
+}
+
+export default Footer;
+    
