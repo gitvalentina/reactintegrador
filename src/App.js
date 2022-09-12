2e9321249ff327ed5304955b3379f4792bc1262c
delfinadelgui <80966598+delfinadelgui@users.noreply.github.com>
2022-09-12 11:04:01 -0300

    not found

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import {Route, Switch} from 'react-router-dom';
 function App () {
     return (
       <React.Fragment>
-<<<<<<< HEAD
       <Header />
       <h1>My App in React</h1>
       <main>       
@@ -26,22 +25,6 @@ function App () {
         </Switch>
       </main>
       <Footer />
-=======
-        <Header />
-        <div>
-          <h1>My App in React</h1>
-          <main>       
-            <Switch>
-              <Route path='/' exact={true} component={Home}/>
-              <Route path='/favoritos' exact component={Favoritos}/>
-              <Route path='/peliculas/id/:id' component={UnaPelicula}/>
-              <Route path='/peliculas' component={Peliculas}/>
-              <Route path='' component={NotFound}/>
-            </Switch>
-          </main>
-        </div>
-        <Footer />
->>>>>>> e54ced37927d32662931c875172996116f5bf5d7
     </React.Fragment>
     );
   }
