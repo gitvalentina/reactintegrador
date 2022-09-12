6e0dcab16dc0568e1e92fb42661fb327ed3bd294
gitvalentina <vdouce@udesa.edu.ar>
2022-09-12 10:59:25 -0300

    header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './Header.css'
 
 function Header(){
 
     return (
-        <nav>
-            <img src="img/logo.jpg" alt="logo" className='logo' />
-            <ul className="main-nav">
-                <li>
-                   <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/peliculas'>All Movies</Link>
-                </li>
-                <li>
-                   <Link to='/favoritos'>Favorites</Link>
-                </li>
-            </ul>
-
-            <ul className="user">
-                <li>
-                    <p>Bienvenido usuario</p>
-                    <img src="/img/user.jpg" alt=""/>
-                </li>
-            </ul>
-        </nav>
+    <>
+            <div className='Header'>
+                <ul className="navegador">
+                    <li className="comp-nav" id="alinear-foto">
+                            <img src="logo/logo.jpeg" alt="Logo StreamTime" className="logo" id="logo" />
+                    </li>
+                    <li className="comp-nav">
+                        <Link to='/'> Home </Link>
+                    </li>
+                    <li className="comp-nav">
+                        <Link to='/favoritos'> Favoritos </Link>
+                    </li>
+                </ul>
+            </div>
+    </>  
     )
 }
-
 export default Header;
