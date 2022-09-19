dbf6a03c62df6bb0dc43bd09526dbbe5cbe98c97
delfinadelgui <80966598+delfinadelgui@users.noreply.github.com>
2022-09-19 14:34:08 -0300

    otro cambios

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import UnaPelicula from "./screens/UnaPelicula/UnaPelicula";
 import NotFound from "./screens/NotFound/NotFound";
 import VerTodasCartelera from "./screens/VerTodasCartelera/VerTodasCartelera";
 import VerTodasPopulares from "./screens/VerTodasPopulares/VerTodasPopulares";
-import {Route, Switch} from 'react-router-dom';
 import Buscador from "./Components/Buscador/Filtrado";
 
+import {Route, Switch} from 'react-router-dom'; //importamos componentes
 
 function App () {
     return (
@@ -19,7 +19,7 @@ function App () {
         <div>
           <main>       
             <Switch>
-              <Route path='/' exact={true} component={Home}/>
+              <Route path='/' exact={true} component={Home}/> 
               <Route path='/favoritos' exact component={Favoritos}/>
               <Route path='/id/:id' component={UnaPelicula}/>
               <Route path='/populares' component={VerTodasPopulares}/>
